8a3b1b47630208be8df71797e271018a57ac73aa
arjusmoon <arju@schbang.com>
2021-06-21 03:40:38 +0530

    update: fix muteAudio & Video

--- background.js
+++ background.js
@@ -18,3 +18,65 @@ async function init() {
     // append/get elements to the DOM
     document.getElementById("webcam-container").appendChild(webcam.canvas);
 }
+
+async function loop(timestamp) {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+function muteAudio(probability) {
+    if (probability >= 1) {
+        const audioButton = document.querySelectorAll(".VfPpkd-Bz112c-LgbsSe")[0];
+        if (audioButton.classList.contains("HNeRed")) {
+            audioButton.click();
+        }
+    }
+}
+
+function turnVideoOff(probability) {
+    if (probability >= 1) {
+        const videoButton = document.querySelectorAll(".VfPpkd-Bz112c-LgbsSe")[1];
+        if (videoButton.classList.contains("HNeRed")) {
+            videoButton.click();
+        }
+    }
+}
+
+async function predict() {
+    const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
+    const prediction = await model.predict(posenetOutput);
+    var predictionsArray = prediction.map(function (o, i) {
+        return { probability: o.probability.toFixed(2), event: o.className }
+    })
+
+    var i;
+    var min = predictionsArray[0].probability
+    var max = predictionsArray[0].probability
+    var event = predictionsArray[0].className;
+    var value;
+    for (i = 1; i < predictionsArray.length; i++) {
+        value = predictionsArray[i].probability
+        if (value < min) min = value;
+        if (value > max) max = value;
+    }
+    const index = predictionsArray.findIndex((list) => {
+        return list.probability == max;
+    })
+    event = predictionsArray[index].event;
+
+    console.log(event, max);
+    if (event === "Mute") {
+        muteAudio(max);
+    } else if (event === "Video") {
+        turnVideoOff(max);
+    }
+}
+
+
+const webcamContainer = document.createElement("div");
+webcamContainer.id = "webcam-container";
+// webcamContainer.style.display = "none"; // Don't display the webcam on the site
+document.body.appendChild(webcamContainer);
+
+init();
